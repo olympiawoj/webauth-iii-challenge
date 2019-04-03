89ce7c197476424157903dca995531b638f8fbed
olympia <owojcik7@gmail.com>
2019-04-03 18:29:06 -0400

    Add token to login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -28,8 +28,9 @@ router.post("/login", async (req, res) => {
     if (username && password) {
       console.log("yes");
       const user = await Users.findBy({ username });
+      const token = generateToken(user);
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.username}` });
+        res.status(200).json({ message: `Welcome ${user.username}`, token });
       } else {
         res.status(401).json({ message: "Invalid credentials" });
       }
@@ -44,3 +45,16 @@ router.post("/login", async (req, res) => {
 });
 
 //LOGOUT
+
+//GENERATE TOKEN FUNCTION
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const secret = "keep it on the low";
+  const options = {
+    expiresIn: "1d"
+  };
+  return jwt.sign(payload, secret, options);
+}
