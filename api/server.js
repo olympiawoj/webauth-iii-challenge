00b9a044c984f8d22dadb7322144f12c8b2300a6
olympia <owojcik7@gmail.com>
2019-04-03 17:31:47 -0400

    Add ability to get users

--- api/server.js
+++ api/server.js
@@ -4,12 +4,13 @@ const server = express();
 
 //import routers here
 const authRouter = require("../auth/auth-router.js");
-
+const usersRouter = require("../users/users-router.js");
 //use middleware here
 server.use(express.json());
 
 //user routers here
 server.use("/api/auth", authRouter);
+server.use("/api", usersRouter);
 
 server.get("/", (req, res) => {
   res.send("server workin aiiight aiiiight");
